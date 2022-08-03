2600fcec08fbcb558cbedf3bfe53dd585f2fa371
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-08-03 11:19:07 +0100

    roboto font add

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { SignIn } from './src/screens/SignIn';
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
   return (
     <NativeBaseProvider theme={THEME}>
       <SignIn/>
-    </NativeBaseProvider>
-    
+    </NativeBaseProvider>   
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,12 @@
-import {View, Text} from 'react-native'
+import { Heading, VStack } from "native-base";
+
 
 export function SignIn(){
   return(
-    <View>
-      <Text>Testandowww</Text>
-    </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+    </VStack>
   )
 }
