7170d5c7ce3b0113bf77b50f538ab286a79e7fce
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-08-05 22:54:15 +0100

    finalizada aula 1

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,10 +3,15 @@ import Logo from '../assets/logo_primary.svg'
 import { Input } from "../components/Input";
 import { Envelope, Key } from 'phosphor-react-native';
 import { Button } from "../components/Button";
+import { useState } from "react";
 
 export function SignIn(){
 
   const {colors} = useTheme();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSignIn() {}
 
   return(
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -18,17 +23,21 @@ export function SignIn(){
         mb={4}
         placeholder="E-mail"
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-        
+        onChangeText={setEmail}
       />
+
       <Input
         mb={8}
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
+
       <Button
         title="Entrar"
         w="full"
+        onPress={handleSignIn}
       />
     </VStack>
   )
