d753ec70a81f3e76100c0802888a9e81fae65627
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-08-07 10:56:46 +0100

    finalizada aula 3

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
-import { NativeBaseProvider,StatusBar } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { SignIn } from './src/screens/SignIn';
+
 import { THEME } from './src/styles/theme';
+
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
-
 export default function App() {
-
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
-      { fontsLoaded ? <SignIn/> : <Loading/> }
-    </NativeBaseProvider>   
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,29 +1,35 @@
-import { Heading, Icon, VStack, useTheme } from "native-base";
-import Logo from '../assets/logo_primary.svg'
-import { Input } from "../components/Input";
+import { useState } from 'react';
+import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
-import { Button } from "../components/Button";
-import { useState } from "react";
 
-export function SignIn(){
+import Logo from '../assets/logo_primary.svg';
 
-  const {colors} = useTheme();
-  const [email, setEmail] = useState('');
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+
+export function SignIn() {
+  const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleSignIn() {}
+  const { colors } = useTheme();
 
-  return(
+  function handleSignIn() {
+    console.log(name, password);
+  }
+
+  return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-      <Logo/>
+      <Logo />
+
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
       </Heading>
+
       <Input
         mb={4}
         placeholder="E-mail"
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-        onChangeText={setEmail}
+        onChangeText={setName}
       />
 
       <Input
@@ -34,11 +40,7 @@ export function SignIn(){
         onChangeText={setPassword}
       />
 
-      <Button
-        title="Entrar"
-        w="full"
-        onPress={handleSignIn}
-      />
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
     </VStack>
   )
 }
