199c5a328e62ebe9de00f6a97509f96be6a61d86
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-08-07 11:51:49 +0100

    imports fix

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { THEME } from './src/styles/theme';
 
-import { Routes } from './src/routes';
+import { THEME } from './src/styles/theme';
+import { Routes } from './src/routes/Index';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
